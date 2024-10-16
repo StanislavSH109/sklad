a1eb478228577243944080599999c783a81c38f7
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-10-16 20:31:13 +0300

    +++

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function getText() {
+    const titleElement = document.createElement('h1');
+    titleElement.textContent = 'Добавить запись';
+    titleElement.classList.add('form__title');
+    return titleElement
+}
+
 function createContainer() {
     const containerElement = document.createElement('div');
     containerElement.classList.add('container');
@@ -5,22 +12,32 @@ function createContainer() {
 }
 
 function createForm() {
-    const formContainer = createContainer();
     const formElement = document.createElement('form');
-    
+    formElement.classList.add('form');
+    return formElement;
 }
 
-
 function addItem() {
+    const appElement = document.querySelector('.add-app');
+    const containerElement = createContainer();
+    const titleElement = getText();
+    const formElement = createForm();
     const buttonAdd = document.querySelector('.button-add');
+
+    // let nameInput = document.createElement('')
 
     buttonAdd.addEventListener('click', function (e) {
         e.preventDefault();
         const tableElement = document.querySelector('.table-section');
         tableElement.classList.add('table-section-none');
     });
+
+    containerElement.append(titleElement, formElement);
+    appElement.append(containerElement, );
+
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function (e) {
     addItem();
-});
+})
+
