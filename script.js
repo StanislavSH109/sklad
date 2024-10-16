2c3ff6cde8567292967119d16285764db6672495
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-10-16 15:14:58 +0300

    +++

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function createContainer() {
+    const containerElement = document.createElement('div');
+    containerElement.classList.add('container');
+    return containerElement
+}
+
+function createForm() {
+    const formContainer = createContainer();
+    const formElement = document.createElement('form');
+    
+}
+
+
 function addItem() {
     const buttonAdd = document.querySelector('.button-add');
 
@@ -7,7 +20,6 @@ function addItem() {
         tableElement.classList.add('table-section-none');
     });
 }
-
 
 document.addEventListener('DOMContentLoaded', function () {
     addItem();
