e0ab1327d4b1b19974c9d12da105fdeb71d0c1f6
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-10-18 09:56:17 +0300

    Created button and date input

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getText() {
     const titleElement = document.createElement('h1');
     titleElement.textContent = 'Добавить запись';
     titleElement.classList.add('form__title');
-    return titleElement
+    return titleElement;
 }
 
 function createInputsElements(type, name, placeholder, id, required) {
@@ -12,13 +12,18 @@ function createInputsElements(type, name, placeholder, id, required) {
     inputElement.id = id;
     inputElement.placeholder = placeholder;
     inputElement.required = required;
-    return inputElement
+
+    if(type == 'number') {
+        inputElement.min = '0';
+    }
+
+    return inputElement;
 } 
 
 function createContainer() {
     const containerElement = document.createElement('div');
     containerElement.classList.add('container');
-    return containerElement
+    return containerElement;
 }
 
 function createForm() {
@@ -27,16 +32,26 @@ function createForm() {
     return formElement;
 }
 
+function buttonCreateWrite(text) {
+    const buttonCreate = document.createElement('button');
+    buttonCreate.textContent = text;
+    buttonCreate.classList.add('form__button-create');
+    return buttonCreate;
+}
+
 function addItem() {
     const appElement = document.querySelector('.add-app');
     const containerElement = createContainer();
     const titleElement = getText();
     const formElement = createForm();
     const buttonAdd = document.querySelector('.button-add');
+    const buttonCreate = buttonCreateWrite('Создать запись');
+
 
     let nameInputElement = createInputsElements('text', 'item-name', 'Название', 'item', 'required');
     let shelfInputElement = createInputsElements('text', 'shelf-number', 'Полка', 'shelf', 'required');
-    let weightInputElement = createInputsElements('')
+    let weightInputElement = createInputsElements('number', 'weight-item', 'Вес', 'weight', 'required');
+    let dateInputElement = createInputsElements('date', 'date-item', 'Дата назначения', 'date', 'required');
 
     buttonAdd.addEventListener('click', function (e) {
         e.preventDefault();
@@ -46,9 +61,11 @@ function addItem() {
     });
 
     containerElement.append(titleElement, formElement);
-    formElement.append(nameInputElement, shelfInputElement);
+    formElement.append(nameInputElement, shelfInputElement, weightInputElement, dateInputElement, buttonCreate);
 
 }
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
     addItem();
